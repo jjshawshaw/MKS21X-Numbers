7df1860f703349ffb8cad31b204d74eb7bf935d7
jjshawshaw <jshaw00@stuy.edu>
2018-10-16 14:51:12 -0400

    Copied format from website

--- RealNumber.java
+++ RealNumber.java
@@ -1,17 +1,51 @@
+
+
 public class RealNumber{
   private double value;
 
-  public RealNumber(double value){
-    this.value = value;
+  public RealNumber(double v){
+    value = v;
   }
+
+  /*
+  *Return the sum of this and the other
+  */
+  public double add(RealNumber other){
+
+     //other can be ANY RealNumber, including a RationalNumber
+
+     //or other subclasses of RealNumber (that aren't written yet)
+
+     return getValue() + other.getValue();
+  }
+
+  /*
+  *Return the product of this and the other
+  */
+  public double multiply(RealNumber other){
+    return getValue() * other.getValue();
+  }
+
+  /*
+  *Return the this divided by the other
+  */
+  public double divide(RealNumber other){
+    return getValue() / other.getValue();
+  }
+
+  /*
+  *Return the this minus the other
+  */
+  public double subtract(RealNumber other){
+    return getValue() - other.getValue();
+  }
+
 
   public double getValue(){
     return value;
   }
 
   public String toString(){
-    return "" + value;
+    return ""+value;
   }
-
-
 }
