b2a66266c19deef53e47fd06cac32f40a7fe12a7
jjshawshaw <jshaw00@stuy.edu>
2018-10-17 23:19:25 -0400

    Completed lab

--- RationalNumber.java
+++ RationalNumber.java
@@ -1,44 +1,54 @@
 public class RationalNumber extends RealNumber
 {
-  private int numerator, denominator;
+  private int nume, deno;
 
   /**Initialize the RationalNumber with the provided values
   *  if the denominator is 0, make the fraction 0/1 instead
   *@param nume the numerator
   *@param deno the denominator
   */
+
   public RationalNumber(int nume, int deno){
     super(0.0);//this value is ignored!
+    if (deno == 0) {
+      this.nume = 0;
+      this.deno = 1;
+    }
+    else{
+    this.nume = nume;
+    this.deno = deno;
+    reduce();
+  }
   }
 
   public double getValue(){
-    return 0.0;
+    return  ((double) nume) / deno;
   }
 
   /**
   *@return the numerator
   */
   public int getNumerator(){
-    return 0;
+    return nume;
   }
   /**
   *@return the denominator
   */
   public int getDenominator(){
-    return 0;
+    return deno;
   }
   /**
   *@return a new RationalNumber that has the same numerator
   *and denominator as this RationalNumber but reversed.
   */
   public RationalNumber reciprocal(){
-    return null;
+    return new RationalNumber(deno, nume);
   }
   /**
   *@return true when the RationalNumbers have the same numerators and denominators, false otherwise.
   */
   public boolean equals(RationalNumber other){
-    return false;
+    return nume == other.getNumerator() && deno == other.getDenominator();
   }
 
 
@@ -46,7 +56,9 @@ public class RationalNumber extends RealNumber
   *@return the value expressed as "3/4" or "8/3"
   */
   public String toString(){
-    return "0";
+    if (deno == 1) return "" + nume;
+    if (nume == 0) return "0";
+    return nume + "/" + deno;
   }
 
   /**Calculate the GCD of two integers.
@@ -56,8 +68,15 @@ public class RationalNumber extends RealNumber
   */
   private static int gcd(int a, int b){
     /*use euclids method or a better one*/
-    http://sites.math.rutgers.edu/~greenfie/gs2004/euclid.html
-    return 0;
+    //http://sites.math.rutgers.edu/~greenfie/gs2004/euclid.html
+    int minAB = Math.min(Math.abs(a), Math.abs(b));
+    int gcd = 1;
+    if (minAB != 0) {
+      for (int i = 1; i <= minAB; i++){
+        if (a % i == 0 && b % i == 0) gcd = i;
+      }
+    }
+    return gcd;
   }
 
   /**
@@ -66,33 +85,39 @@ public class RationalNumber extends RealNumber
   *reduced after construction.
   */
   private void reduce(){
-
+    if (deno < 0) {
+      nume = -nume;
+      deno = -deno;
+    }
+    int gcd = gcd(nume, deno);
+    nume /= gcd;
+    deno /= gcd;
   }
   /******************Operations Return a new RationalNumber!!!!****************/
   /**
   *Return a new RationalNumber that is the product of this and the other
   */
   public RationalNumber multiply(RationalNumber other){
-    return null;
+    return new RationalNumber(nume * other.getNumerator(), deno * other.getDenominator());
   }
 
   /**
   *Return a new RationalNumber that is the this divided by the other
   */
   public RationalNumber divide(RationalNumber other){
-    return null;
+    return multiply(other.reciprocal());
   }
 
   /**
   *Return a new RationalNumber that is the sum of this and the other
   */
   public RationalNumber add(RationalNumber other){
-    return null;
+    return new RationalNumber(nume * other.getDenominator() + deno * other.getNumerator(), deno * other.getDenominator());
   }
   /**
   *Return a new RationalNumber that this minus the other
   */
   public RationalNumber subtract(RationalNumber other){
-    return null;
+    return new RationalNumber(nume * other.getDenominator() - deno * other.getNumerator(), deno * other.getDenominator());
   }
-} 
+}
